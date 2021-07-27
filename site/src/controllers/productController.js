f2e4917d0d1af2bf7ec88af1a475f9b6afbbd4c5
LorenaCoheneBaez <loreley.cb15@gmail.com>
2021-07-27 20:52:59 -0300

    Agrego header-admin, addProducts, editProduct.ejs y modifico admin.ejs

--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -15,6 +15,16 @@ module.exports = {
         {title: 'LEAF | Administrador'})
     },
 
+    editarProducto: (req,res) => {
+        return res.render("./products/editProduct",
+        {title: 'LEAF | Administrador'})
+    },
+    agregarProducto: (req,res) => {
+        return res.render("./products/addProduct",
+        {title: 'LEAF | Administrador'})
+    },
+
+
     carrito: (req,res) => {
         return res.render("./products/productCart",
         {title: 'LEAF | Carrito'})
