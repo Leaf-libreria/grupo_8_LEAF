0063950607a0b05d60536d9f45e74940b31f8665
LorenaCoheneBaez <loreley.cb15@gmail.com>
2021-07-30 19:47:36 -0300

    Corrijo errores de clave en products.json, se itera administrador

--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -1,49 +1,49 @@
-const fs = require('fs');
-const path = require('path');
-const productos = require('../data/product_db');
+const fs = require("fs");
+const path = require("path");
+const productos = require("../data/product_db");
 
 module.exports = {
-    libros: (req,res) => {
-        return res.render("./products/libros",
-        {title: 'LEAF | Libros',
-        productos,
-        papel : productos.filter(producto => producto.formato === "Libro"),})
+  libros: (req, res) => {
+    return res.render("./products/libros", {
+      title: "LEAF | Libros",
+      productos,
+      papel: productos.filter((producto) => producto.formato === "Libro"),
+    });
+  },
 
-    },
+  verMas: (req, res) => {
+    return res.render("verMas", { title: "LEAF | Libros", productos });
+  },
 
-    verMas: (req,res) => {
-        return res.render("verMas",{title: 'LEAF | Libros',
-        productos})
+  detail: (req, res) => {
+    return res.render("./products/productDetail", { title: "LEAF | Detalle" });
+  },
 
-    },
+  administrador: (req, res) => {
+    return res.render("./products/admin", {
+      title: "LEAF | Administrador",
+      productos,
+    }); //Lista todos los productos
+  },
 
-    detail: (req,res) => {
-        return res.render("./products/productDetail",
-        {title: 'LEAF | Detalle'})
-    },
+  editarProducto: (req, res) => {
+    return res.render("./products/editProduct", {
+      title: "LEAF | Administrador",
+    });
+  },
+  agregarProducto: (req, res) => {
+    return res.render("./products/addProduct", {
+      title: "LEAF | Administrador",
+    });
+  },
 
-    administrador: (req,res) => {
-        return res.render("./products/admin",
-        {title: 'LEAF | Administrador'})
-    },
+  carrito: (req, res) => {
+    return res.render("./products/productCart", { title: "LEAF | Carrito" });
+  },
 
-    editarProducto: (req,res) => {
-        return res.render("./products/editProduct",
-        {title: 'LEAF | Administrador'})
-    },
-    agregarProducto: (req,res) => {
-        return res.render("./products/addProduct",
-        {title: 'LEAF | Administrador'})
-    },
-
-
-    carrito: (req,res) => {
-        return res.render("./products/productCart",
-        {title: 'LEAF | Carrito'})
-    },
-
-    pago: (req,res) => {
-        return res.render("./products/payForm",
-        {title: 'LEAF | Finaliza tu compra'})
-    }
-}
+  pago: (req, res) => {
+    return res.render("./products/payForm", {
+      title: "LEAF | Finaliza tu compra",
+    });
+  },
+};
