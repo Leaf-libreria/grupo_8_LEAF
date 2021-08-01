058a99058e42d008b00d008ee38c685b9d241d8a
AntonelaEspinola <antoespinola55@gmail.com>
2021-08-01 00:39:55 -0300

    Se crearon la vistas ejs para cada uno de los generos, sus rutas y metodos para mostrar contenido dinamicamente

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -1,16 +1,25 @@
+const productos = require("../data/product_db");
+const generos = require('../data/generos_db')
 module.exports = {
     index: (req,res) => {
         return res.render("index",
-        {title: 'LEAF'})
+        { title: "LEAF",
+        productos,
+        generos,
+    })
     },
 
     preguntas: (req,res) =>{
         return res.render("preguntasFrecuentes",
-        {title: 'LEAF | Preguntas frecuentes'})
+        {title: 'LEAF | Preguntas frecuentes',
+        generos,
+    })
     },
 
     quienesSomos: (req,res) =>{
         return res.render("quienesSomos",
-        {title: 'LEAF | Quiénes somos'})
+        {title: 'LEAF | Quiénes somos',
+        generos,
+    })
     }
 }
--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const productos = require("../data/product_db");
+const generos = require('../data/generos_db')
 
 module.exports = {
   libros: (req, res) => {
@@ -8,21 +9,34 @@ module.exports = {
       title: "LEAF | Libros",
       productos,
       papel: productos.filter((producto) => producto.formato === "Libro"),
+      generos,
+    });
+  },
+  ebooks: (req, res) => {
+    return res.render("./products/ebooks", {
+      title: "LEAF | E-books",
+      productos,
+      ebooks: productos.filter((producto) => producto.formato === "E-book"),
+      generos,
     });
   },
 
   verMas: (req, res) => {
-    return res.render("verMas", { title: "LEAF | Libros", productos });
+    return res.render("verMas", { title: "LEAF | Libros", productos },
+    generos,);
+
   },
 
   detail: (req, res) => {
-    return res.render("./products/productDetail", { title: "LEAF | Detalle" });
+    return res.render("./products/productDetail", { title: "LEAF | Detalle" },
+    generos,);
   },
 
   administrador: (req, res) => {
     return res.render("./products/admin", {
       title: "LEAF | Administrador",
       productos,
+      generos,
     }); //Lista todos los productos
   },
 
@@ -34,16 +48,93 @@ module.exports = {
   agregarProducto: (req, res) => {
     return res.render("./products/addProduct", {
       title: "LEAF | Administrador",
+      generos,
     });
   },
 
   carrito: (req, res) => {
-    return res.render("./products/productCart", { title: "LEAF | Carrito" });
+    return res.render("./products/productCart", { title: "LEAF | Carrito" },
+    generos,
+    );
   },
 
   pago: (req, res) => {
     return res.render("./products/payForm", {
       title: "LEAF | Finaliza tu compra",
+      generos,
+    });
+  },
+  // controladores para generos
+  policial :  (req, res) => {
+    return res.render("./products/generos/policial", {
+      title: "LEAF | Policial",
+      productos,
+      policial: productos.filter((producto) => producto.genero === "Policial"),
+      generos,
+    });
+  },
+  romance : (req,res) => {
+    return res.render("./products/generos/romance",{
+      title : "LEAF | Romance",
+      productos,
+      romance : productos.filter((producto) => producto.genero === 'Romance'),
+      generos,
+    });
+  },
+  misterio : (req,res) => {
+    return res.render("./products/generos/misterio",{
+      title : "LEAF | Misterio",
+      productos,
+      misterio : productos.filter((producto) => producto.genero === 'Misterio'),
+      generos,
+    });
+  },
+  terror : (req,res) => {
+    return res.render("./products/generos/terror",{
+      title : "LEAF | Terror",
+      productos,
+      terror : productos.filter((producto) => producto.genero === 'Terror'),
+      generos,
+    });
+  },
+  ficcion : (req,res) => {
+    return res.render("./products/generos/ficcion",{
+      title : "LEAF | Ficcion",
+      productos,
+      ficcion : productos.filter((producto) => producto.genero === 'Ficcion'),
+      generos,
+    });
+  },
+  cienciaFiccion : (req,res) => {
+    return res.render("./products/generos/ciencia-ficcion",{
+      title : "LEAF | Ciencia-ficcion",
+      productos,
+      cienciaFiccion : productos.filter((producto) => producto.genero === 'Ciencia-ficcion'),
+      generos,
+    });
+  },
+  juvenil : (req,res) => {
+    return res.render("./products/generos/juvenil",{
+      title : "LEAF | Juvenil",
+      productos,
+      juvenil : productos.filter((producto) => producto.genero === 'Juvenil'),
+      generos,
+    });
+  },
+  historica : (req,res) => {
+    return res.render("./products/generos/historica",{
+      title : "LEAF | Historica",
+      productos,
+      historica : productos.filter((producto) => producto.genero === 'Historica'),
+      generos,
+    });
+  },
+  novela : (req,res) => {
+    return res.render("./products/generos/novela",{
+      title : "LEAF | Novela",
+      productos,
+      novela : productos.filter((producto) => producto.genero === 'Novela'),
+      generos,
     });
   },
 };
