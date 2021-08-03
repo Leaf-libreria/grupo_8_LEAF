393340464ff27c182fe2de9651171e7ed257238d
AntonelaEspinola <antoespinola55@gmail.com>
2021-08-02 23:46:50 -0300

    Se creo dinamicamente la lista desplegable de generos

--- site/src/routes/productsRoute.js
+++ site/src/routes/productsRoute.js
@@ -1,12 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const {verMas, detail, administrador, carrito, pago, agregarProducto, editarProducto, libros, ebooks, policial, romance, terror, misterio, historica, ficcion, cienciaFiccion, novela, juvenil} = require("../controllers/productController")
+const {
+  verMas,
+  detail,
+  administrador,
+  carrito,
+  pago,
+  agregarProducto,
+  editarProducto,
+  libros,
+  ebooks,
+  policial,
+  romance,
+  terror,
+  misterio,
+  historica,
+  ficcion,
+  cienciaFiccion,
+  novela,
+  juvenil,
+} = require("../controllers/productController");
 
 // /products
-router.get('/ebooks', ebooks);
-router.get('/libros', libros);
-router.get('/', verMas);
+// ruta de ebooks
+router.get("/ebooks", ebooks);
+// ruta de libros formato papel
+router.get("/libros", libros);
+router.get("/", verMas);
 router.get("/detalle", detail);
 router.get("/administrador", administrador);
 router.get("/agregar", agregarProducto);
@@ -14,13 +35,14 @@ router.get("/editar", editarProducto);
 router.get("/carrito", carrito);
 router.get("/pago", pago);
 // rutas de generos
-router.get('/misterio', misterio);
-router.get('/terror', terror);
-router.get('/romance', romance);
-router.get('/historica', historica);
-router.get('/ficcion', ficcion);
-router.get('/ciencia-ficcion', cienciaFiccion);
-router.get('/policial', policial);
-router.get('/novela', novela);
-router.get('/juvenil', juvenil)
+router.get("/misterio", misterio);
+router.get("/terror", terror);
+router.get("/romance", romance);
+router.get("/historica", historica);
+router.get("/ficcion", ficcion);
+router.get("/ciencia-ficcion", cienciaFiccion);
+router.get("/policial", policial);
+router.get("/novela", novela);
+router.get("/juvenil", juvenil);
+
 module.exports = router;
