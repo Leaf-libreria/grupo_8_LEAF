112969d62d05433523a8e045a658baf310ee2de0
AntonelaEspinola <antoespinola55@gmail.com>
2021-08-20 10:31:40 -0300

    Se hizo dinamica la vista perfil

--- site/src/routes/usersRoute.js
+++ site/src/routes/usersRoute.js
@@ -6,6 +6,6 @@ const {login, registro, perfil, editarPerfil} = require("../controllers/userCont
 
 router.get('/login', login);
 router.get("/register", registro);
-router.get('/perfil', perfil);
+router.get('/perfil/:id', perfil);
 router.get('/editarPerfil/:id', editarPerfil)
 module.exports = router;
