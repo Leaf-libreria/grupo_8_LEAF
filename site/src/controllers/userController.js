da5cedde8c3c0c64d18b87afde916149e54a9caa
LorenaCoheneBaez <loreley.cb15@gmail.com>
2021-08-22 20:46:01 -0300

    Agrega validaciones para login

--- site/src/controllers/userController.js
+++ site/src/controllers/userController.js
@@ -1,9 +1,25 @@
+const { validationResult } = require('express-validator');
+const { usuarios } = require('../data/users.json');
+
 module.exports = {
     login: (req,res) => {
         return res.render("./users/login",
         {title: 'LEAF | Login'})
     },
 
+    loginUsuario:(req,res)=>{
+        let errors = validationResult(req);
+        let { email } = req.body;
+        if (errors.isEmpty()) {
+            return res.redirect('/');
+        }else{
+            return res.render('./users/login', {
+              errores: errors.mapped(),
+              old: req.body,
+              title: 'LEAF | Login',
+            });
+        }
+},
     registro: (req,res) =>{
         return res.render("./users/register",
         {title: 'LEAF | Registro'})
--- site/src/routes/usersRoute.js
+++ site/src/routes/usersRoute.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {login, registro} = require("../controllers/userController")
-
+const {login, registro,loginUsuario} = require("../controllers/userController")
+const loginValidator=require('../validations/loginValidator')
 
 router.get('/login', login);
+router.post('/login', loginValidator, loginUsuario);
 router.get("/register", registro);
 
 module.exports = router;
