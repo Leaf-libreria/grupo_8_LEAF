126ef907618c90665f20106cba5d310304bf44e7
Eliana995 <eliana.ag.95@gmail.com>
2021-08-22 18:43:56 -0300

    se implemento multer para usuarios

--- site/src/routes/usersRoute.js
+++ site/src/routes/usersRoute.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const multer = require("multer");
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null,"public/images")
+    },
+    filename: (req,file,cb) => {
+        cb(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    },
+})
+
+
+var upload = multer({storage : storage});
+
 
 const {login, registro, perfil, editarPerfil} = require("../controllers/userController")
 
