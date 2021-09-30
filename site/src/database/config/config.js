7869906230677b32b19fc7a2e04bc654b318ceda
AntonelaEspinola <antoespinola55@gmail.com>
2021-09-30 19:08:26 -0300

    Se aplico en el proyecto variables de entorno

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: '',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: "leaf_db",
     host: "127.0.0.1",
     dialect: "mysql"
