417a7c5638c9ad8a0250c96aa2f65b24dcbe826b
Eliana995 <eliana.ag.95@gmail.com>
2021-10-01 15:25:07 -0300

    se unificaron scripts

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -9,7 +11,7 @@ module.exports = {
   test: {
     username: "root",
     password: null,
-    database: "database_test",
+    database: "leaf_db",
     host: "127.0.0.1",
     dialect: "mysql"
   },
